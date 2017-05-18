c933457499592b0d883745f4dec167c82cb6e695
jsheerin66 <justinsheerin@gmail.com>
2017-05-18 19:12:19 -0400

    final commit
    got to the point where logic needs to be inserted to find best match.
    Need to go back at a later date to finish up.

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var friends = [{
   "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores": [3, 3, 5, 1, 1, 4, 1, 3, 2, 3]
 }, {
-  "name": "Becky",
+  "name": "Kelly",
   "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores": [4, 1, 5, 2, 5, 3, 2, 5, 5, 2]
 }, {
